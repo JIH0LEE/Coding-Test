3e32c502039b9c5df7a84c344cfa3589c138527a
Eugine Park <pkeugine@gmail.com>
2022-07-04 21:58:28 +0900

    유진 큰 수 만들기 추가

---
+++ pkeugine/CreateBigNumber.java
@@ -0,0 +1,31 @@
+public class CreateBigNumber {
+
+    public static void main(String[] args){
+
+        Solution solution = new Solution();
+
+         System.out.println(solution.solution("1924", 2));
+         System.out.println(solution.solution("1231234", 3));
+         System.out.println(solution.solution("4177252841", 4));
+    }
+}
+
+class Solution {
+
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        int max = 0;
+        for (int i = 0; i < number.length() - k; i++) {
+            max = 0;
+            for (int j = index; j <= k + i; j++) {
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    index = j + 1;
+                }
+            }
+            sb. append(max);
+        }
+        return sb.toString();
+    }
+}
